d7198d347bf707f15d4fe77b2e01bd85844813c8
ZPatriot-cyber <makssergeij@gmail.com>
2026-02-27 13:38:32 +0300

    Создана базовая структура проекта

---
+++ Models/ProcessInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProcessManagerUltimate.Models
+{
+    public class ProcessInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ProcessPriorityClass Priority { get; set; }
+        public long MemoryUsage { get; set; }
+        public int ThreadCount { get; set; }
+        public TimeSpan CpuTime { get; set; }
+        public bool Responding { get; set; }
+    }
+}
---
+++ Models/ProcessNode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ProcessManagerUltimate.Models
+{
+    public class ProcessNode
+    {
+        public ProcessInfo Process { get; set; }
+        public ObservableCollection<ProcessNode> Children { get; set; }
+            = new ObservableCollection<ProcessNode>();
+    }
+}
