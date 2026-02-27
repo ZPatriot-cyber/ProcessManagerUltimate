d965b29f572a3308e02571de167f91eb483db7c3
ZPatriot-cyber <makssergeij@gmail.com>
2026-02-27 13:33:18 +0300

    Models
    Реализованы модели данных для процессов, потоков и иерархии процессов

---
+++ Models/ThreadInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProcessManagerUltimate.Models
+{
+    public class ThreadInfo
+    {
+        public int Id { get; set; }
+        public ThreadPriorityLevel Priority { get; set; }
+        public ThreadState State { get; set; }
+        public TimeSpan CpuTime { get; set; }
+    }
+}
