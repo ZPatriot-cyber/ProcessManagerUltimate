3856ab92b2b9fc285f5f4b357ea69269792ce4d4
ZPatriot-cyber <makssergeij@gmail.com>
2026-03-27 01:31:50 +0300

    Add files via upload

---
+++ services/CpuMonitorService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ProcessManagerUltimate.Services
+{
+    public class CpuMonitorService
+    {
+        private readonly List<PerformanceCounter> counters;
+
+        public CpuMonitorService()
+        {
+            counters = new List<PerformanceCounter>();
+            int cores = System.Environment.ProcessorCount;
+
+            for (int i = 0; i < cores; i++)
+            {
+                counters.Add(new PerformanceCounter("Processor", "% Processor Time", i.ToString()));
+            }
+        }
+
+        public List<float> GetUsage()
+        {
+            var list = new List<float>();
+
+            foreach (var c in counters)
+            {
+                list.Add(c.NextValue());
+            }
+
+            return list;
+        }
+    }
+}
