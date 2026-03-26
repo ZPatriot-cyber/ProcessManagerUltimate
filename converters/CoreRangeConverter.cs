2e2696b0c869a948cfd6352ae69efbb091a15dee
ZPatriot-cyber <makssergeij@gmail.com>
2026-03-27 01:32:48 +0300

    Add files via upload

---
+++ converters/CoreRangeConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ProcessManagerUltimate.Converters
+{
+    public class CoreRangeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int coreCount && coreCount > 0)
+                return $"Ядра (0-{coreCount - 1})";
+            return "Ядра";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
