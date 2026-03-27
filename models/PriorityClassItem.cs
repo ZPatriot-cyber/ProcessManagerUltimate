8ee6aafbc9aaab1c6c13f7787fd7cd8586bce2dd
ZPatriot-cyber <makssergeij@gmail.com>
2026-03-27 18:18:34 +0300

    Добавить базовые модели данных
    - ProcessInfo: модель процесса с привязкой к ViewModel, поддержка выделения, приоритета, affinity
       - ThreadInfo: информация о потоках
       - ProcessNode: узел дерева процессов
       - PriorityClassItem: элемент для выпадающего списка приоритетов

---
+++ models/PriorityClassItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+namespace ProcessManagerUltimate.Models
+{
+    public class PriorityClassItem
+    {
+        public string Name { get; set; }
+        public ProcessPriorityClass Value { get; set; }
+    }
+}
