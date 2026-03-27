48e3f009eaca7636baba541596d73999d7cbfb46
ZPatriot-cyber <makssergeij@gmail.com>
2026-03-27 18:21:55 +0300

    Добавить ViewModel'и (BaseViewModel, MainViewModel)
     - MainViewModel: основная логика приложения, таймер обновления, коллекции процессов, потоков, дерева, топ-10 по памяти
       - График CPU через LiveCharts, поддержка выбора процессов и массовых операций

---
+++ viewmodels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace ProcessManagerUltimate.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
