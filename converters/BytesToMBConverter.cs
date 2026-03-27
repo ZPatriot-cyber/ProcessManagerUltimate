fe6c2f94372ef479f911ba229e0cf4ad7a0a9e93
ZPatriot-cyber <makssergeij@gmail.com>
2026-03-27 18:17:15 +0300

    Добавить базовые модели данных
    - ProcessInfo: модель процесса с привязкой к ViewModel, поддержка выделения, приоритета, affinity
       - ThreadInfo: информация о потоках
       - ProcessNode: узел дерева процессов
       - PriorityClassItem: элемент для выпадающего списка приоритетов

---
+++ converters/BytesToMBConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ProcessManagerUltimate.Converters
+{
+    public class BytesToMBConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is long bytes)
+            {
+                double mb = bytes / (1024.0 * 1024.0);
+                return mb.ToString("N2") + " MB";
+            }
+
+            return "0 MB";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ converters/PriorityToBrushConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ProcessManagerUltimate.Converters
+{
+    public class PriorityToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ProcessPriorityClass p)
+            {
+                if (p == ProcessPriorityClass.RealTime)
+                    return Brushes.Red;
+                if (p == ProcessPriorityClass.High)
+                    return Brushes.Orange;
+                if (p == ProcessPriorityClass.AboveNormal)
+                    return Brushes.Yellow;
+                if (p == ProcessPriorityClass.Normal)
+                    return Brushes.Transparent;
+                if (p == ProcessPriorityClass.BelowNormal)
+                    return Brushes.LightBlue;
+                if (p == ProcessPriorityClass.Idle)
+                    return Brushes.LightGray;
+            }
+
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
